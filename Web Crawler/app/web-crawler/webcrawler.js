5d9dae8dbf57130486eb9bba0edb42e9887d4bcf
Ricardo Carvalho Godinho <primeit014@ctt.pt>
2018-07-09 16:04:07 +0100

    First commit. Simple crawling to a webpage and extract the stock Exchange value of CTT company.

---
+++ Web Crawler/app/web-crawler/webcrawler.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var Nightmare = require('nightmare');
+require('nightmare-iframe-manager')(Nightmare);
+
+
+var app = express();
+
+app.get('/', (req, res) => {
+	
+	
+	var nightmare = Nightmare({
+//		  show:true,  
+		  waitTimeout: 5000,
+//		  running: true,
+		  webPreferences: {
+		    webSecurity:false
+		    }
+		  });
+	nightmare.goto('https://www.ctt.pt/ctt-e-investidores/acao-ctt/cotacao.html')
+			 .wait('#iframe_cotacao')
+			 .enterIFrame('#iframe_cotacao')
+			 .wait('#Ticker8 > table > tbody > tr.TickerRow.OddRow.Ticker_OddRow.Ticker_SelectedRow > td.TickerValueTD.TickerValueTD_LastPrice > span')
+			 .evaluate(function() {
+				 return document.querySelector('#Ticker8 > table > tbody > tr.TickerRow.OddRow.Ticker_OddRow.Ticker_SelectedRow > td.TickerValueTD.TickerValueTD_LastPrice > span').textContent;
+			 })
+			 .end()
+			 .then(function (result) {
+				 
+				 console.log('O valor das acções CTT atual é: ' + result +'€\n'); 
+				 res.send("O valor das acções CTT atual é: "+result+ "€");
+			 })
+			 .catch(function (error) {
+				 console.error('Search failed:', error); 
+				 res.send("ERROR");
+			 });
+	
+
+	
+});
+
+
+app.listen(3000, () => console.log("Listening on port 3000"));
+
+
+
